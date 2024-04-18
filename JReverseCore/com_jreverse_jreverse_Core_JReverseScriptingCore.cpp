f1f1c3456fe817a59a307501a6b762873218a666
Aaron Wilk <aaron.wilk@icloud.com>
2024-04-18 00:17:41 -0500

    impl way to use dif classloader?

--- JReverseCore/com_jreverse_jreverse_Core_JReverseScriptingCore.cpp
+++ JReverseCore/com_jreverse_jreverse_Core_JReverseScriptingCore.cpp
@@ -19,6 +19,59 @@ heap_callback(jlong class_tag, jlong size, jlong* tag_ptr, void* user_data) {
     return JVMTI_ITERATION_CONTINUE;
 }
 
+const char* CheckJNIErrorCooler(JNIEnv* jniEnv) {
+    jthrowable ex = nullptr;
+    if (jniEnv->ExceptionCheck()) { std::cout << "A JVM EXEPTION WAS FOUND!" << std::endl; ex = jniEnv->ExceptionOccurred(); }
+    if (ex == nullptr) return "No Error";
+
+    jclass exceptionClass = jniEnv->GetObjectClass(ex);
+
+    // Get the method ID of the getMessage() method of Throwable
+    jmethodID getMessageMethod = jniEnv->GetMethodID(exceptionClass, "getMessage", "()Ljava/lang/String;");
+
+    // Call getMessage() method
+    jstring messageObj = (jstring)jniEnv->CallObjectMethod(ex, getMessageMethod);
+
+    // Convert messageObj to C string
+    const char* message = jniEnv->GetStringUTFChars(messageObj, nullptr);
+    std::cout << "Exception message: " << message << std::endl;
+
+    // Create a StringWriter to capture the stack trace
+    jclass stringWriterClass = jniEnv->FindClass("java/io/StringWriter");
+    jmethodID stringWriterConstructor = jniEnv->GetMethodID(stringWriterClass, "<init>", "()V");
+    jobject stringWriterObject = jniEnv->NewObject(stringWriterClass, stringWriterConstructor);
+    jmethodID stringWriterMethod = jniEnv->GetMethodID(stringWriterClass, "toString", "()Ljava/lang/String;");
+
+    // Create a PrintWriter to write to the StringWriter
+    jclass printWriterClass = jniEnv->FindClass("java/io/PrintWriter");
+    jmethodID printWriterConstructor = jniEnv->GetMethodID(printWriterClass, "<init>", "(Ljava/io/Writer;)V");
+    jobject printWriterObject = jniEnv->NewObject(printWriterClass, printWriterConstructor, stringWriterObject);
+
+    // Print the stack trace to the PrintWriter
+    jclass throwableClass = jniEnv->FindClass("java/lang/Throwable");
+    jmethodID printStackTraceMethod = jniEnv->GetMethodID(throwableClass, "printStackTrace", "(Ljava/io/PrintWriter;)V");
+    jniEnv->CallVoidMethod(ex, printStackTraceMethod, printWriterObject);
+
+    // Get the string representation from the StringWriter
+    jstring stackTraceString = (jstring)jniEnv->CallObjectMethod(stringWriterObject, stringWriterMethod);
+
+    // Convert the Java string to C-style string
+    const char* stackTraceCString = jniEnv->GetStringUTFChars(stackTraceString, NULL);
+
+    // Print the stack trace string to std::cout
+    std::cout << "Exception stack trace:\n" << stackTraceCString << std::endl;
+
+    // Release the allocated memory
+    jniEnv->ReleaseStringUTFChars(stackTraceString, stackTraceCString);
+
+    // Release the message string
+    jniEnv->ReleaseStringUTFChars(messageObj, message);
+
+    jniEnv->ExceptionDescribe();
+    jniEnv->ExceptionClear();
+    return message;
+}
+
 
 JNIEXPORT jobjectArray JNICALL Java_com_jreverse_jreverse_Core_JReverseScriptingCore_GetClassInstances(JNIEnv* jniEnv, jclass, jstring ClassName)
 {
@@ -26,7 +79,6 @@ JNIEXPORT jobjectArray JNICALL Java_com_jreverse_jreverse_Core_JReverseScripting
     std::thread::id this_id = std::this_thread::get_id();
     std::cout << "GetClassInstance thread: " << this_id << std::endl;
     std::cout << "Starting Native Registration!" << std::endl;
-
 
     std::printf("Getting TIEnv Pointer!\n");
 
@@ -57,6 +109,7 @@ JNIEXPORT jobjectArray JNICALL Java_com_jreverse_jreverse_Core_JReverseScripting
     std::cout << "Checking Existance of: " << clazz.c_str() << std::endl;
 
     jclass cladd = globalenv->FindClass(clazz.c_str()); //Does not find some loaded classes for some reason.
+    CheckJNIErrorCooler(globalenv);
     jclass stringclass = globalenv->FindClass("java/lang/String");
     if (stringclass == nullptr) { std:printf("Class Finding Does not work!"); }
     if (cladd == nullptr) {
